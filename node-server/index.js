3186247cf1cf1c767720af7237046ad05cdbe107
Isra O <Isra@israadam1993.com>
2019-05-27 14:14:31 -0500

    Angular app built

--- node-server/index.js
+++ node-server/index.js
@@ -1,18 +1,26 @@
+//import { TweetsComponent } from './scr/app/tweets';
 var Twit = require('twit');
 var config = require('./config.js');
 var Twitter = new Twit(config);
-
+const path = require('path');
 const express = require('express')();
 const app = express; 
+var serve = require('express-static')
+
 
 function  tweetSearch(){
     var params = {
-      q: 'Trump',
+      q: 'Trump', 
       result_type: 'recent',
       lang: 'en'    
     } 
     return Twitter.get('search/tweets', params);
 }
+
+app.get('/', (request, response) => {
+    response.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+app.use(serve(__dirname + '/dist'));
 
 
 app.get('/api/tweets', (req, res) => {
@@ -21,5 +29,6 @@ app.get('/api/tweets', (req, res) => {
     });
 });
 
-app.listen(3000);
+
+app.listen(3000, () => console.log("Hello"));
 
